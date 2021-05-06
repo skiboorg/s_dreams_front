2c3a135933005d22096994303069ce08b8373f99
skiboorg <d@skib.org>
2021-05-06 11:33:42 +0300

    fix thanks page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -891,15 +891,7 @@ export default {
 
     const script = document.createElement("script");
     script.innerText= `
- !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '757475264939457');
+ fbq('init', '757475264939457');
   fbq('track', 'PageView');
   fbq('track', 'ViewContent');
     `;
